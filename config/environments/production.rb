b7a3eb914c20b66898aaeab2303f8279d60c1ac6
Ryan Everett <reverett4994@gmail.com>
2019-12-02 03:54:15 -0500

    test 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,13 +30,7 @@ Rails.application.configure do
 
 
   config.serve_static_assets = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
